854cb4b108e8efbc4e92739bbc5ceb1d1887ad3b
dreehgoncalves <andressagoncalves1997@gmail.com>
2021-09-16 20:10:53 -0300

    first commit

---
+++ cadastrar.php
@@ -0,0 +1,17 @@
+<?php
+
+include("conexao.php");
+
+  if ($_POST) {
+    extract($_POST);
+    $sql = "INSERT INTO contato (nome, email, motivo, mensagem) values ('$nome', '$email', '$motivo', '$mensagem')";
+
+    $res = mysqli_query($con, $sql);
+
+    if ($res == false) {
+      die ("Erro ao inserir usuario");
+    } else {
+      die ("Usuario inserido com sucesso");
+    }
+  }
+?>
---
+++ conexao.php
@@ -0,0 +1,7 @@
+<?php
+    $con = mysqli_connect('localhost','root','','bd_contato');
+
+    if($con == false){
+        die("Erro ao conectar com bd");
+    }
+?>
