6ec280b608d47d066ad32f6e72992549e57aee8d
dreehgoncalves <andressagoncalves1997@gmail.com>
2021-09-16 20:43:33 -0300

    criação

--- cadastrar.php
+++ cadastrar.php
@@ -4,6 +4,7 @@ include("conexao.php");
 
   if ($_POST) {
     extract($_POST);
+    
     $sql = "INSERT INTO contato (nome, email, motivo, mensagem) values ('$nome', '$email', '$motivo', '$mensagem')";
 
     $res = mysqli_query($con, $sql);
